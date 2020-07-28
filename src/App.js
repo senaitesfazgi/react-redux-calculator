4de3608d81ddb789877e51b539bc361c17be8319
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-07-28 07:39:13 -0600

    Adding numbers and getting result

--- src/App.js
+++ src/App.js
@@ -6,70 +6,59 @@ class App extends React.Component{
       this.state = {
         num1:"",
         num2:"",
-        result:""
+        result:"",
       };
     }
+    add(){
+      console.log("Num1: ", this.state.num1, " - Num2: ", this.state.num2)
+      this.setState({ result: parseInt(this.state.num1) + parseInt(this.state.num2) })
+      console.log(this.state)
 
-chnageNum = (event) =>
-{
-  event.preventDefault();
-  const num1 = {
-    num1:this.state.num1
-  };
-  const num2 = {
-    num2:this.state.num2
-  };
-  console.log(num1);
-  console.log(num2);
-}
-updateItem(key, value)
-{
-  this.setState({[key]:value});
-}
+    }
     
-render()
-{
-  return(
-    <>
-    <form>
-      <h1> Calculator App!</h1>
-      Input first number:
-      <br/>
-      <input type="text" 
-              name = "num1"
-              id="num1"
-              required 
-              value={this.state.num1}
-              onChange={event => this.updateItem('num1', event.target.value)} 
-      />
-      <br/>
-      <br/>
-      Choose Operation:
-      <br/>
-      <select value = {this.state.operation}>
-        <option value = {this.state.operation}> + </option>
-        <option value = {this.state.operation}> - </option>
-        <option value = {this.state.operation}> X </option>
-        <option value ={this.state.operation}> / </option>
-      </select>
-      <br/>
-      <br/>
-      Input second number:
-      <br/>
-      <input type="text" 
-              name = "num2"
-              id="num2"
-              required 
-              value={this.state.num2}
-              onChange={event => this.updateItem('num2', event.target.value)} />
-      <br/>
-      <br/>
-      <input type="submit" value="Calculate"/>
+  render()
+  {
+    return(
+      <>
+      <form>
+        <h1> Calculator App!</h1>
+        Input first number:
+        <br/>
+        <input type="text" 
+                name = "num1"
+                id="num1"
+                required 
+                value={this.state.num1}
+                onChange={ (eve) => { this.setState({ num1: eve.target.value }) } } 
+        />
+        <br/>
+        <br/>
+        Choose Operation:
+        <br/>
+        <select value = {this.state.operation}>
+          <option value = {this.state.operation}> + </option>
+          <option value = {this.state.operation}> - </option>
+          <option value = {this.state.operation}> X </option>
+          <option value ={this.state.operation}> / </option>
+        </select>
+        <br/>
+        <br/>
+        Input second number:
+        <br/>
+        <input type="text" 
+                name = "num2"
+                id="num2"
+                required 
+                value={this.state.num2}
+                onChange={ (eve) => { this.setState({ num2: eve.target.value }) } }/>
+        <br/>
+        <br/>
+        <input type="submit"  value="Calculate" onClick = {() => this.add()}/>
+        <h1>Result:{this.state.result}</h1>
 
-    </form>
-    </>
-  )
+      </form>
+      </>
+    )
+  }
 }
-}
-
 export default App;
