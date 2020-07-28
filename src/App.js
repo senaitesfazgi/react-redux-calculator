6b94c430ce1d343c9188ed18ca599e43bea99e83
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-07-28 07:49:46 -0600

    Adding of numbers working fine

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends React.Component{
         result:"",
       };
     }
-    add(){
+    add=(event) => {
+      event.preventDefault();
       console.log("Num1: ", this.state.num1, " - Num2: ", this.state.num2)
       this.setState({ result: parseInt(this.state.num1) + parseInt(this.state.num2) })
       console.log(this.state)
@@ -20,7 +21,7 @@ class App extends React.Component{
   {
     return(
       <>
-      <form>
+      <form onSubmit = {this.add}>
         <h1> Calculator App!</h1>
         Input first number:
         <br/>
@@ -53,7 +54,7 @@ class App extends React.Component{
                 onChange={ (eve) => { this.setState({ num2: eve.target.value }) } }/>
         <br/>
         <br/>
-        <input type="submit"  value="Calculate" onClick = {() => this.add()}/>
+        <input type="submit"  value="Calculate" onClick = {() => this.add}/>
         <h1>Result:{this.state.result}</h1>
 
       </form>
